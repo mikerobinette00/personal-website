94a5f575ca46adccd1f5f7e8323e583103e12de0
mikerobinette00 <122401832+mikerobinette00@users.noreply.github.com>
2024-06-18 18:43:42 -0400

    fixing repo location
    fixing repo location

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://mikerobinette.dev',
+  base: "/personal-website"
 });

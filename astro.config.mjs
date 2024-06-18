992b6134c8b2fc141a71d69fe8bacb3478ea1acf
mikerobinette00 <122401832+mikerobinette00@users.noreply.github.com>
2024-06-18 18:16:17 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  site: 'https://astronaut.github.io',
+  base: 'personal-website',
 });

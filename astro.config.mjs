2a7e5eb79cdd3a70b58749c17c51ff345642742b
mikerobinette00 <122401832+mikerobinette00@users.noreply.github.com>
2024-06-18 18:21:22 -0400

    added CNAME file

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://mikerobinette00.github.io',
-  base: 'personal-website',
+  site: 'https://mikerobinette.dev',
 });
